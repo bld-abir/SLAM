7c487897453ee1a18c8401e299cdc4bb3c27529c
BLAD Abir <53100788+bld-abir@users.noreply.github.com>
2021-06-08 21:36:57 +0100

    Update ScanMatchingExample_ICP.m

--- ScanMatchingExample_ICP.m
+++ ScanMatchingExample_ICP.m
@@ -1,14 +1,7 @@
 clc
 clear all
 close all
-% Ça cartographie super bien jusqu'à la fin. L'inconvénient réside dans le
-% fait que la trajctoire ne commence pas à 0, et que la carte le monde
-% dérive d'une transormation (translaiton et rotation inconnus). On aura
-% donc besoin de travailler avec les positions relatives au lieu des poses
-% absolues. Et pour se faire, nous allons tenter d'utiliser la fonction
-% exampleHelperComposeTransform ou du moins la partie qui gère la
-% conversion des poses relatives en poses absolues et on va l'inverser si
-% possible.
+
 %% Load Laser Scan Data from File
 filePath = fullfile(fileparts(mfilename('fullpath')), 'data', 'scanMatchingData.mat');
 load(filePath);
@@ -43,38 +36,32 @@ hold off
 drawnow
 
 %% Build Occupancy Grid Map Using Iterative Scan Matching
-map = robotics.OccupancyGrid(30, 30, 5);
-map.GridLocationInWorld = [-20 -20]%origine of map
+map = robotics.OccupancyGrid(15, 15, 5);
+map.GridLocationInWorld = [-7.5 -7.5]%origine of map
 
 numScans = numel(laserMsg);
-initialPose = [0 0 0];
-poseList = zeros(numScans,3);
-poseList(1,:) = initialPose;
-transform = initialPose;
+% initialPose = [13.9011 8.6925 0];
+% poseList = zeros(numScans,3);
+% poseList(1,:) = initialPose;
+% transform = initialPose;
 %%
 for idx = 6:numScans
     % Process the data in pairs.
-    referenceScan = lidarScan(laserMsg{idx-5})
-    referenceScanMsg = laserMsg{idx-5};
+    referenceScan = lidarScan(laserMsg{idx-2})
+    referenceScanMsg = laserMsg{idx-2};
     currentScanMsg = laserMsg{idx};
     currentScan = lidarScan(currentScanMsg);
 
     [Rmat, Tvec] = icp2(referenceScan.Cartesian, currentScan.Cartesian,5,10,4);
     alpha = asin(Rmat(2,1));
-    %alpha = atan2(Rmat(2,1),Rmat(1,1));
-%     qA(idx,1)=odomMsg{idx}.Pose.Pose.Orientation.X;
-%     qA(idx,2)=odomMsg{idx}.Pose.Pose.Orientation.Y;
-%     qA(idx,3)=odomMsg{idx}.Pose.Pose.Orientation.Z;
-%     qA(idx,4)=odomMsg{idx}.Pose.Pose.Orientation.W;
-%     eul(idx,:)=quat2eul(qA(idx,:));
-%     alpha(idx)=eul(idx,3)
-    transform=[Tvec(1),Tvec(2), alpha];
+%     alpha = atan2(Rmat(2,1),Rmat(1,1));
+%     transform=[Tvec(1),Tvec(2), alpha];
 
 %     absolutePose = exampleHelperComposeTransform(poseList(idx-5,:), transform);
 %     poseList(idx,:) = absolutePose;
 %        
-    odomList(idx,1)=odomMsg{idx}.Pose.Pose.Position.X 
-    odomList(idx,2)=odomMsg{idx}.Pose.Pose.Position.Y 
+    odomList(idx,1)=odomMsg{idx}.Pose.Pose.Position.X;
+    odomList(idx,2)=odomMsg{idx}.Pose.Pose.Position.Y;
     
     qA(idx,1)=odomMsg{idx}.Pose.Pose.Orientation.X;
     qA(idx,2)=odomMsg{idx}.Pose.Pose.Orientation.Y;
@@ -82,26 +69,34 @@ for idx = 6:numScans
     qA(idx,4)=odomMsg{idx}.Pose.Pose.Orientation.W;
     eul(idx,:)=quat2eul(qA(idx,:));
     alphao(idx)=eul(idx,3)
-    alpha=alphao'
+    alpha=alphao';
     
-    absolutePose=[odomList(idx,1)-odomList(6,1),odomList(idx,2)-odomList(6,2), alpha(idx)];
-    insertRay(map, absolutePose, referenceScan, double(currentScanMsg.RangeMax));
- 
-%     insertRay(map, absolutePose, referenceScan, double(currentScanMsg.RangeMax));
+    angle = alphao(6);
+    P = [odomList(:,1)-odomList(6,1) odomList(:,2)-odomList(6,2)];
+    R = [cos(angle) -sin(angle);
+        sin(angle)   cos(angle)];
+    PP = P * R ;
+    % On réiitilise les cases de 1 à 5 qui ont toutes les mêmes valeurs que
+    % celles e la case 6. Sin on les laisse les 5 premiers points se
+    % retrouvent à [-PP(6,1) -PP(6,2)]
+    PP(1:5,1:2)=0 ;
     
-    
+    absolutePose=[PP(idx,1), PP(idx,2), alpha(idx)-alphao(6)];
+    insertRay(map, absolutePose, currentScan, double(currentScanMsg.RangeMax));
+     
+
     idx
        %% Visualize Map
     figure(6)
     show(map);
     title('Occupancy grid map built using scan matching results');
     hold on
-    plot(odomList(:,1)-odomList(6,1),odomList(:,2)-odomList(6,2), 'b', 'DisplayName', 'Estimated robot position');
-    legend('show', 'Location', 'NorthWest')
-    %xlim=([6,690])
-    hold off 
-    drawnow
 
+    plot(PP(:,1), PP(:,2), 'b', 'DisplayName', 'Estimated robot position');
+    legend('show', 'Location', 'NorthWest')
+    
+    hold off 
+    drawnow 
 end
 
 displayEndOfDemoMessage(mfilename)
